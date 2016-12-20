b822fef4e68beaed565b323aae4b000b95f4035a
KCrason <535089696@qq.com>
2016-12-20 18:12:41 +0800

    推荐数据构建完成

---
+++ app/src/main/java/site/krason/focusdaily/activities/VideoActivity.java
@@ -0,0 +1,23 @@
+package site.krason.focusdaily.activities;
+
+/**
+ * @author Created by KCrason on 2016/12/20.
+ * @email 535089696@qq.com
+ */
+
+public class VideoActivity extends  BaseActivity{
+    @Override
+    protected boolean isExistToolbar() {
+        return false;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+}
